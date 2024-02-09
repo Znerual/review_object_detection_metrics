46571fbd3ad0e76b02afc58ac76ac4b098e2caac
Laurenz Ruzicka <Laurenz.Ruzicka@ait.ac.at>
2024-02-09 13:41:32 +0100

    .

--- src/object_detection_metrics/evaluators/pascal_voc_evaluator.py
+++ src/object_detection_metrics/evaluators/pascal_voc_evaluator.py
@@ -5,8 +5,8 @@ from collections import Counter
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from src.bounding_box import BoundingBox
-from src.utils.enumerators import (BBFormat, CoordinatesType,
+from object_detection_metrics.bounding_box import BoundingBox
+from object_detection_metrics.utils.enumerators import (BBFormat, CoordinatesType,
                                    MethodAveragePrecision)
 
 
